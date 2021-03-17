8c2914778f6dcb718fae3a88d25db91fe8e7d057
takasyo <dummy.address.0128@gmail.com>
2021-03-18 04:19:51 +0900

    Add load_basis()

--- script/load_data.py
+++ script/load_data.py
@@ -2,7 +2,6 @@ import csv
 
 def load_ability():
     # 特殊能力
-
     with open("./data/special_ability.csv") as f:
         reader = csv.reader(f)
         l = [row for row in reader]
@@ -14,16 +13,34 @@ def load_ability():
         data[i] = list(map(float, data[i]))
 
     # 下位を合わせた計算
-    for i in range(len(data["得能"])):
+    for i in range(len(l[0]) - 1):
         if data["下位"][i] != "":
             for j in ["筋力", "敏捷", "技術", "変化", "精神", "査定"]:
-                data[j][i] += data[j][i-1]
+                data[j][i] += data[j][i-1] if i-1 >= 0 else data[j][0]
 
     return data
 
 def load_basis():
     # 基礎能力
-    pass
+    basis = {}
+    for num in range(7):
+        with open(f"./data/{num}.csv") as f:
+            reader = csv.reader(f)
+            l = [row for row in reader]
+            l = [list(x) for x in zip(*l)]
+
+        data={l[i][0]:l[i][1:] for i in range(len(l))}
+
+        for i in ["筋力", "敏捷", "技術", "変化", "精神", "査定", "÷7.84"]:
+            data[i] = list(map(float, data[i]))
+
+        for i in range(len(l[0]) - 1):
+            for j in ["筋力", "敏捷", "技術", "変化", "精神"]:
+                data[j][i] += data[j][i-1] if i-1 >= 0 else data[j][0]
+        
+        basis[l[0][0]] = data
+
+    return basis
 
 def load_extend():
     # 拡張能力
