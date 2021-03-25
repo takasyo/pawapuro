03ab605085280c25a3a70dbd1bf7f334764fadce
takasyo <dummy.address.0128@gmail.com>
2021-03-26 02:34:03 +0900

    Add extend function

--- script/calcmax.py
+++ script/calcmax.py
@@ -1,9 +1,10 @@
 import pulp
 
-from load_data import load_ability, load_basis, load_extend
+from load_data import load_ability, load_basis
 
 ability = load_ability()
 basis = load_basis()
+extend = load_basis("extend")
 
 # 問題の定義
 prob = pulp.LpProblem(name="パワプロ", sense=pulp.LpMaximize)
@@ -11,18 +12,20 @@ prob = pulp.LpProblem(name="パワプロ", sense=pulp.LpMaximize)
 # 変数の定義
 xs = [pulp.LpVariable('{}'.format(x), cat=pulp.LpBinary) for x in ability["得能"]]
 ys = [[pulp.LpVariable('{}{}'.format(name, x), cat=pulp.LpBinary) for x in basis[name][name]] for name in basis.keys()]
-# zs = [extend]拡張能力
+zs = [[pulp.LpVariable('{}{}'.format(name, x), cat=pulp.LpBinary) for x in extend[name][name]] for name in extend.keys()]
 
 # 目的関数
 xx = pulp.lpDot(ability["査定"], xs)
 yy = 7.84*(pulp.lpDot([basis[name]["÷7.84"] for name in basis.keys()], ys)*7/6 + 0.5)
-prob += pulp.lpSum(xx + yy)
+zz = pulp.lpDot([extend[name]["査定"] for name in extend.keys()], zs)
+prob += pulp.lpSum(xx + yy + zz)
 
 # 制約条件の定義
 for j in ["筋力", "敏捷", "技術", "変化", "精神"]:
     x0 = pulp.lpDot(ability[j], xs)
     y0 = pulp.lpSum([pulp.lpDot(basis[name][j], ys[i]) for i, name in enumerate(basis.keys())])
-    prob += pulp.lpSum(x0+y0) <= 1000
+    z0 = pulp.lpSum([pulp.lpDot(extend[name][j], zs[i]) for i, name in enumerate(extend.keys())])
+    prob += pulp.lpSum(x0 + y0 + z0) <= 5000
 
 # 下位得能の対応
 flag = 0
@@ -37,6 +40,9 @@ for i in range(len(ability["得能"])):
 
 for i in range(len(ys)):
     prob += pulp.lpSum(ys[i]) <= 1
+
+for i in range(len(zs)):
+    prob += pulp.lpSum(zs[i]) <= 1
 
 # 互換得能の対応
 for i in range(len(ability["得能"])):
@@ -60,7 +66,15 @@ for tmp in ys:
     for y in tmp:
         if y.value() != 0:
             print(f"{y}:{y.value()}")
+for tmp in zs:
+    for z in tmp:
+        if z.value() != 0:
+            print(f"{z}:{z.value()}")
 
-# print(f"\n査定：{pulp.lpDot(ability['査定'], xs).value()}")
-# for key in ["筋力", "敏捷", "技術", "変化", "精神"]:
-#     print(f"{key}：500 に対し {pulp.lpDot(ability[key], xs).value()}")
+print(f"\n査定：{pulp.lpSum(xx + yy + zz).value()}")
+
+for j in ["筋力", "敏捷", "技術", "変化", "精神"]:
+    x0 = pulp.lpDot(ability[j], xs)
+    y0 = pulp.lpSum([pulp.lpDot(basis[name][j], ys[i]) for i, name in enumerate(basis.keys())])
+    z0 = pulp.lpSum([pulp.lpDot(extend[name][j], zs[i]) for i, name in enumerate(extend.keys())])
+    print(f"{j}：5000に対し {pulp.lpSum(x0 + y0 + z0).value()}")
--- script/load_data.py
+++ script/load_data.py
@@ -20,11 +20,17 @@ def load_ability():
 
     return data
 
-def load_basis():
+def load_basis(set="basis"):
     # 基礎能力
+    maxlen = 7
+    prefix = "b"
+    if set == "extend":
+        maxlen=2
+        prefix="e"
+
     basis = {}
-    for num in range(7):
-        with open(f"./data/b{num}.csv") as f:
+    for num in range(maxlen):
+        with open(f"./data/{prefix}{num}.csv") as f:
             reader = csv.reader(f)
             l = [row for row in reader]
             l = [list(x) for x in zip(*l)]
@@ -34,6 +40,7 @@ def load_basis():
         for i in ["筋力", "敏捷", "技術", "変化", "精神", "査定", "÷7.84"]:
             data[i] = list(map(float, data[i]))
 
+        # 下位を合わせた計算
         for i in range(len(l[0]) - 1):
             for j in ["筋力", "敏捷", "技術", "変化", "精神"]:
                 data[j][i] += data[j][i-1] if i-1 >= 0 else data[j][0]
@@ -41,7 +48,3 @@ def load_basis():
         basis[l[0][0]] = data
 
     return basis
-
-def load_extend():
-    # 拡張能力
-    pass
