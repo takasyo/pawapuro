c56c1a3537c83188d651c3c4cf41e55fa23b333f
takasyo <dummy.address.0128@gmail.com>
2021-04-15 03:52:34 +0900

    Integrated load_file

--- script/load_data.py
+++ script/load_data.py
@@ -1,8 +1,8 @@
 import csv
 
-def load_ability(sense):
-    # 特殊能力
-    with open("./data/ability.csv") as f:
+def load_file(sense, filename):
+    # ファイル読み込み
+    with open(filename) as f:
         reader = csv.reader(f)
         l = [row for row in reader]
         l = [list(x) for x in zip(*l)]
@@ -12,15 +12,25 @@ def load_ability(sense):
     for i in ["筋力", "敏捷", "技術", "変化", "精神", "査定"]:
         data[i] = list(map(float, data[i]))
 
+    # センス〇
     if sense:
         for i in ["筋力", "敏捷", "技術", "変化", "精神"]:
             data[i] = list(map(lambda x: int(x*0.9), data[i]))
 
+    return data
+
+def load_ability(sense):
+    # 特殊能力
+    data = load_file(sense, "./data/ability.csv")
+
     # 下位を合わせた計算
-    for i in range(len(l[0]) - 1):
+    for i in range(len(data["査定"])):
         if data["下位"][i] != "":
             for j in ["筋力", "敏捷", "技術", "変化", "精神", "査定"]:
-                data[j][i] += data[j][i-1] if i-1 >= 0 else data[j][0]
+                data[j][i] += data[j][i-1]
+
+    # 四捨五入
+    data["査定"] = list(map(lambda x: round(x, 2), data["査定"]))
 
     return data
 
@@ -34,25 +44,15 @@ def load_basis(sense, set="basis"):
 
     basis = {}
     for num in range(maxlen):
-        with open(f"./data/{prefix}{num}.csv") as f:
-            reader = csv.reader(f)
-            l = [row for row in reader]
-            l = [list(x) for x in zip(*l)]
+        data = load_file(sense, f"./data/{prefix}{num}.csv")
 
-        data={l[i][0]:l[i][1:] for i in range(len(l))}
-
-        for i in ["筋力", "敏捷", "技術", "変化", "精神", "査定", "÷7.84"]:
-            data[i] = list(map(float, data[i]))
-
-        if sense:
-            for i in ["筋力", "敏捷", "技術", "変化", "精神"]:
-                data[i] = list(map(lambda x: int(x*0.9), data[i]))
+        data["÷7.84"] = list(map(float, data["÷7.84"]))
 
         # 下位を合わせた計算
-        for i in range(len(l[0]) - 1):
+        for i in range(len(data["査定"])):
             for j in ["筋力", "敏捷", "技術", "変化", "精神"]:
-                data[j][i] += data[j][i-1] if i-1 >= 0 else data[j][0]
-        
-        basis[l[0][0]] = data
+                data[j][i] += data[j][i-1] if i-1 >= 0 else 0
+
+        basis[list(data.keys())[0]] = data
 
     return basis
